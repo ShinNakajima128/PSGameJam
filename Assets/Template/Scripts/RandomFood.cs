5f6065ae19b3e35a36af70d65dd4ecc92f3e83e8
RYUKI <ryukiarashi@icloud.com>
2021-07-26 14:39:52 +0900

    ランダムで画像を差し替える

---
+++ Assets/Template/Scripts/RandomFood.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RandomFood : MonoBehaviour
+{
+    public Sprite[] sprite;
+    public float m_timer = 5;
+    float time = 0;
+
+    private void Update()
+    {
+        time += Time.deltaTime;
+        RandomImage();
+    }
+    void RandomImage()
+    {
+        var image = GetComponent<Image>();//このオブジェクトにくっ付いてるImageの取得
+        while(m_timer < time)
+        {
+            time = 0;
+            image.sprite = sprite[Random.Range(0, sprite.Length)];//配列からランダムに画像指定
+        }
+    }
+}
