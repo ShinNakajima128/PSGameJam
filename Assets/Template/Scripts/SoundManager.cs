90ebbde0c22dbb36cd384c5a1e8ff59c26ca7ec5
ShinNakajima128 <ukauka224@i.softbank.jp>
2021-07-26 11:41:23 +0900

    タイトル画面を作成した

--- Assets/Template/Scripts/SoundManager.cs
+++ Assets/Template/Scripts/SoundManager.cs
@@ -50,30 +50,30 @@ public class SoundManager : SingletonMonoBehaviour<SoundManager>
 
     private void Start()
     {
-        if (Instance != null)
-        {
-            SceneManager.sceneLoaded += OnSceneLoaded;
-            if (SceneManager.GetActiveScene().name == "Title")
-            {
-                PlayBgmByName("Title");
-            }
-            else if (SceneManager.GetActiveScene().name == "")
-            {
-                PlayBgmByName("");
-            }
-            else if (SceneManager.GetActiveScene().name == "")
-            {
-                PlayBgmByName("");
-            }
-            else if (SceneManager.GetActiveScene().name == "")
-            {
-                PlayBgmByName("");
-            }
-            else if (SceneManager.GetActiveScene().name == "")
-            {
-                PlayBgmByName("");
-            }
-        }  
+        //if (Instance != null)
+        //{
+        //    SceneManager.sceneLoaded += OnSceneLoaded;
+        //    if (SceneManager.GetActiveScene().name == "Title")
+        //    {
+        //        PlayBgmByName("Title");
+        //    }
+        //    else if (SceneManager.GetActiveScene().name == "")
+        //    {
+        //        PlayBgmByName("");
+        //    }
+        //    else if (SceneManager.GetActiveScene().name == "")
+        //    {
+        //        PlayBgmByName("");
+        //    }
+        //    else if (SceneManager.GetActiveScene().name == "")
+        //    {
+        //        PlayBgmByName("");
+        //    }
+        //    else if (SceneManager.GetActiveScene().name == "")
+        //    {
+        //        PlayBgmByName("");
+        //    }
+        //}  
     }
 
     /// <summary>
@@ -85,24 +85,24 @@ public class SoundManager : SingletonMonoBehaviour<SoundManager>
     {
         if (Instance != null)
         {
-            switch (SceneManager.GetActiveScene().name)
-            {
-                case "Title":
-                    PlayBgmByName("Title");
-                    break;
-                //case "":
-                //    PlayBgmByName("");
-                //    break;
-                //case "":
-                //    PlayBgmByName("");
-                //    break;
-                //case "":
-                //    PlayBgmByName("");
-                //    break;
-                //case "":
-                //    PlayBgmByName("");
-                //    break;
-            }
+            //switch (SceneManager.GetActiveScene().name)
+            //{
+            //    case "Title":
+            //        PlayBgmByName("Title");
+            //        break;
+            //    //case "":
+            //    //    PlayBgmByName("");
+            //    //    break;
+            //    //case "":
+            //    //    PlayBgmByName("");
+            //    //    break;
+            //    //case "":
+            //    //    PlayBgmByName("");
+            //    //    break;
+            //    //case "":
+            //    //    PlayBgmByName("");
+            //    //    break;
+            //}
         } 
     }
 
---
+++ Assets/Template/Scripts/TitleManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleManager : MonoBehaviour
+{
+    private void Update()
+    {
+        if (Input.anyKeyDown)
+        {
+            SoundManager.Instance.PlaySeByName("Enter1");
+            LoadSceneManager.Instance.LoadEasyScene();
+        }
+    }
+}
