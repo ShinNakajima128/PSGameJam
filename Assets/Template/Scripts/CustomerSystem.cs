114cbffeb1c07fa3e1aa16985431ec3fea4440c9
RYUKI <ryukiarashi@icloud.com>
2021-07-26 18:01:42 +0900

    no message

--- Assets/Template/Scripts/CustomerSystem.cs
+++ Assets/Template/Scripts/CustomerSystem.cs
@@ -10,24 +10,20 @@ public class CustomerSystem : MonoBehaviour
     /// <summary>true の場合、開始時にまず生成する</summary>
     [SerializeField] bool m_generateOnStart = true;
     [SerializeField] float m_interval = 1;
-    float m_timer;
+    float m_timer = 0;
 
     void Start()
     {
         Instantiate(m_prefab, this.transform.position, Quaternion.identity);
-        if (m_generateOnStart)
-        {
-            m_timer = m_interval;
-        }
     }
 
     void Update()
     {
-        m_interval -= Time.deltaTime;
+        m_timer += Time.deltaTime;
 
         if (m_timer > m_interval)
         {
-            m_interval = 0;
+            m_timer = 0;
             if (m_customer.m_timer != 0)
             {
                 Instantiate(m_prefab, this.transform.position, Quaternion.identity);
