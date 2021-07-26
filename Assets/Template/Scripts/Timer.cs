2cc830cf3ad6804c68e64d69a241e46844013900
RYUKI <ryukiarashi@icloud.com>
2021-07-26 13:11:55 +0900

    時間が0になったら、テキストを表示し、2秒後にシーン切り替えするようにした

---
+++ Assets/Template/Scripts/Timer.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Timer : MonoBehaviour
+{
+    [SerializeField] Slider m_Slider;
+    public GameObject m_ObjectSlider;
+    [SerializeField] float m_Timer = 1;
+    int m_Time = 0;
+
+    [SerializeField] GameObject m_GameEnd;
+
+    void Start()
+    {
+        m_Slider = m_ObjectSlider.GetComponent<Slider>();
+    }
+    void Update()
+    {
+        GameTimer();
+    }
+
+    public void GameTimer()
+    {
+        m_Slider.value = m_Timer;
+        m_Timer -= Time.deltaTime;
+
+        if (m_Timer < m_Time)
+        {
+            m_Timer = 0;
+            m_GameEnd.SetActive(true);
+            StartCoroutine(LoadScene());
+        }
+    }
+
+    public IEnumerator LoadScene()
+    {
+        yield return new WaitForSeconds(2f);
+        LoadSceneManager.Instance.Result();
+    }
+}
