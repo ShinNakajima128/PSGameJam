b2c3f63da99773b0dd2126b4d50b727f01901ced
ShinNakajima128 <ukauka224@i.softbank.jp>
2021-07-28 12:38:07 +0900

    食材と食べ物のPrefabを作成した

---
+++ Assets/RefineFolder/Scripts/FoodData.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "MyScriptable/Create FoodData")]
+public class FoodData : ScriptableObject
+{
+    [SerializeField] string m_foodName = default;
+    [SerializeField] int m_price = default;
+    [SerializeField] List<GameObject> m_foodstuffList = new List<GameObject>();
+
+    public string FoodName
+    {
+        get { return m_foodName; }
+    }
+
+    public int Price
+    {
+        get { return m_price; }
+    }
+
+    public List<GameObject> FoodstuffList
+    {
+        get { return m_foodstuffList; }
+    }
+}
