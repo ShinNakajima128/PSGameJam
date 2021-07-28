b29d90ccba0510da3b6ea4676157e45e78b61ff2
ShinNakajima128 <ukauka224@i.softbank.jp>
2021-07-28 13:34:46 +0900

    食材のScriptableObjectを作成した

---
+++ Assets/RefineFolder/Scripts/FoodstuffData.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "MyScriptable/Create FoodstuffData")]
+public class FoodstuffData : ScriptableObject
+{
+    [SerializeField] string m_stuffName = default;
+    [SerializeField] int m_cost = default;
+    [SerializeField] int m_index = default; 
+
+    public int Cost { get => m_cost; }
+    public int Index { get => m_index; }
+}
